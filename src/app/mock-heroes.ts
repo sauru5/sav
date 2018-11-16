162a9391816ca15bc74c3b97e2af8e4954dbaf95
sauru5 <39490618+sauru5@users.noreply.github.com>
2018-11-15 22:29:28 -0600

    Add files via upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Newton' },
+  { id: 12, name: 'BP Koirala' },
+  { id: 13, name: 'Mahavir Pun' },
+  { id: 14, name: 'Sandeep Lamichhane' },
+  { id: 15, name: 'Ramanujan' },
+  { id: 16, name: 'Baburam' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Prithivi Narayan Shah' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
